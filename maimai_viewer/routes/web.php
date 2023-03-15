102492e549c89a4fbdaaf8d889a5b34a875288b6
Alepha <junhao.lim2002@gmail.com>
2023-03-15 15:41:51 +0800

    added base templates (need further testing)

--- maimai_viewer/routes/web.php
+++ maimai_viewer/routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nav_test', function() {
+    return view('navbar', [
+        'title'=> "Navbar Test",
+        'description'=> "Testing the navbar port",
+        'logo_url'=> secure_asset('images/bearhands.png'),
+        'profile_url'=> secure_asset('images/user_img.png'),
+        'user'=> [
+                'name'=> "H O S H I N O",
+                'title'=> "響け！CHIREI MY WAY!",
+                'rating'=> "15000"
+        ],
+        'status'=>[
+            'achievements'=>'',
+            'songs'=>'',
+            'recommendations'=>''
+        ]
+    ]);
+});
