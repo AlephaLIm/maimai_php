42fcaf0d1bd47b370a498ce4464012417dcd1fb2
Alepha <junhao.lim2002@gmail.com>
2023-03-18 22:30:05 +0800

    added basic model to navbar

---
+++ maimai_viewer/app/Models/Navbar.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models;
+
+
+class Navbar {
+    public $name;
+    public $title;
+    public $rating;
+    public $profile_img;
+    public static function retrieveuser() {
+        $user = new Navbar();
+        $user->name = "H O S H I N O";
+        $user->title = "響け！CHIREI MY WAY!";
+        $user->rating = "15000";
+        $user->profile_img = "/images/nav_icons/user_img.png";
+
+        return $user;
+    }
+};
+
---
+++ maimai_viewer/resources/views/songs.blade.php
@@ -0,0 +1,12 @@
+@extends('navbar', ['title' => $title, 'description' => $description, 'logo_url' => $logo_url, 'user' => $user, 'status' => $status])
+
+@section('head')
+    @parent
+    
+@endsection
+
+@section('body')
+    <main>
+        
+    </main>
+@endsection
