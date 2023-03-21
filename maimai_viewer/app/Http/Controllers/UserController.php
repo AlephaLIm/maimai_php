184615a16af0b6606ab1acbdf800e868980f34d6
brdley <bradleygoh22@gmail.com>
2023-03-21 21:35:04 +0800

    register page design complete

--- maimai_viewer/app/Http/Controllers/UserController.php
+++ maimai_viewer/app/Http/Controllers/UserController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Navbar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use App\Models\Page_status;
 
 class UserController extends Controller
 {
     // register form
     public function create(){
-        return view('users.register');
+        return view('users.register',[
+            'title'=> 'Register',
+            'description'=> "Create a new user",
+            'logo_url'=> URL::asset('/images/nav_icons/bearhands.png'),
+            'user'=> Navbar::retrieveuser(),
+            'status'=>Page_status::set_status('')
+        ]);
     }
+
 
     // create user
     public function store(Request $request){
--- maimai_viewer/resources/views/users/register.blade.php
+++ maimai_viewer/resources/views/users/register.blade.php
@@ -1,62 +1,73 @@
-<header class="text-center">
-<h2 class="text-2xl font-bold uppercase mb-1">Register</h2>
-<p class="mb-4">Create an account to view profile</p>
-</header>
+@extends('navbar', ['title' => $title, 'description' => $description, 'logo_url' => $logo_url, 'user' => $user, 'status' => $status])
 
-<form method="POST" action="/users">
-@csrf
-<div class="mb-6">
-    <label for="friendcode" class="inline-block text-lg mb-2"> Friendcode </label>
-    <input type="text" class="border border-gray-200 rounded p-2 w-full" name="friendcode" value="{{old('friendcode')}}" />
+@section('links')
+    @parent
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/register_test.css') }}" />    
+@endsection
 
-    @error('friendcode')
-    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-    @enderror
-</div>
+@section('body')
+    <div class='bg-limit'>
+        <div class='login_wrapper'>
+            <span class="title">
+                <h1>Register</h1>
+                <h6>Create account to view profile</h6>
+            </span>
+            
+            <form class="login-form" method="POST" action="/users">
+                @csrf
+                <div class="input-box">             
+                    <label for="friendcode" class="label-input"> Friendcode </label>
+                    <span id="icons" class="material-symbols-outlined">pin</span>
+                    <input type="text" class="input-field" name="friendcode" placeholder="Enter friendcode" />
+                
+                    @error('friendcode')
+                    <p class="error-msg">{{$message}}</p>
+                    @enderror
+                </div>
 
-<div class="mb-6">
-    <label for="email" class="inline-block text-lg mb-2">Email</label>
-    <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
-
-    @error('email')
-    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-    @enderror
-</div>
-
-<div class="mb-6">
-    <label for="password" class="inline-block text-lg mb-2">
-    Password
-    </label>
-    <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password"
-    value="{{old('password')}}" />
-
-    @error('password')
-    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-    @enderror
-</div>
-
-<div class="mb-6">
-    <label for="password2" class="inline-block text-lg mb-2">
-    Confirm Password
-    </label>
-    <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password_confirmation"
-    value="{{old('password_confirmation')}}" />
-
-    @error('password_confirmation')
-    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-    @enderror
-</div>
-
-<div class="mb-6">
-    <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-    Sign Up
-    </button>
-</div>
-
-<div class="mt-8">
-    <p>
-    Already have an account?
-    <a href="/login" class="text-laravel">Login</a>
-    </p>
-</div>
-</form>
+                <div class="input-box">
+                    <label for="email" class="label-input"> Email </label>
+                    <span id="icons" class="material-symbols-outlined">mail</span>
+                    <input type="email" class="input-field" name="email" placeholder="Enter email" />
+                
+                    @error('email')
+                    <p class="error-msg">{{$message}}</p>
+                    @enderror
+                </div>
+                
+                <div class="input-box">
+                    <label for="password" class="label-input"> Password </label>
+                    <span id="icons" class="material-symbols-outlined">lock</span>
+                    <input type="password" class="input-field" name="password" placeholder="Enter password" />
+                
+                    @error('password')
+                    <p class="error-msg">{{$message}}</p>
+                    @enderror
+                </div>
+                
+                <div class="input-box">
+                    <label for="password_confirmation" class="label-input"> Password Confirmation </label>
+                    <span id="icons" class="material-symbols-outlined">lock</span>
+                    <input type="password" class="input-field" name="password_confirmation" placeholder="Confirm password" />
+                
+                    @error('password_confirmation')
+                    <p class="error-msg">{{$message}}</p>
+                    @enderror
+                </div>
+                
+                <div class="submit-button-container">
+                    <button type="submit" class="submit-button">
+                    Sign Up
+                    </button>
+                </div>
+                
+                <div class="redirect">
+                    <p>
+                    Already have an account?
+                    <a href="/login" class="redirect">Login</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
