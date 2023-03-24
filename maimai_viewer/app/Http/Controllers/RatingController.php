b0b0ea8776e38bf53e56d6a37399d3dd4ce38de4
Nydrel2 <dariustan.sg@gmail.com>
2023-03-25 02:40:18 +0800

    Update RatingController.php

--- maimai_viewer/app/Http/Controllers/RatingController.php
+++ maimai_viewer/app/Http/Controllers/RatingController.php
@@ -5,9 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Chart;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-use App\Models\RatingSong;
-use App\Models\RatingChart;
-use App\Models\RatingScore;
 
 class RatingController extends Controller
 {
