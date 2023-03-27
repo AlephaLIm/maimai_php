b63d7a16658bcd3e2854b52443d104c42c78ffdd
SimYanZhe <simyanzhe12345@gmail.com>
2023-03-27 15:00:23 +0800

    included AP and AP+

--- maimai_viewer/app/Http/Controllers/ProfileController.php
+++ maimai_viewer/app/Http/Controllers/ProfileController.php
@@ -11,7 +11,7 @@ class ProfileController extends Controller
     {
         //SELECT charts.level, scores.score, scores.scoregrade, scores.combograde from scores inner join charts on scores.chartid=charts.chartid AND scores.friendcode = '7025818836209';
         $user = DB::select('select * from users where friendcode = ?;', [$id]);
-        $data = DB :: select('SELECT charts.level, scores.score, scores.scoregrade, scores.combograde from scores inner join charts on scores.chartid=charts.chartid AND scores.friendcode = ? AND scores.scoregrade LIKE "S%";',[$id]);
+        $data = DB :: select('SELECT charts.level, scores.score, scores.scoregrade, scores.combograde from scores inner join charts on scores.chartid=charts.chartid AND scores.friendcode = ? AND scores.scoregrade LIKE "S%" order by constant DESC;',[$id]);
         $uniqueLevels = array();
         $levelArray = array();
         for ($i=0; $i < count($data); $i++){
@@ -26,6 +26,10 @@ class ProfileController extends Controller
             $SSS_data = $data[0]->SSS;
             $data = DB::select('SELECT count(scores.scoregrade) as "SSSP" from scores inner join charts on scores.chartid=charts.chartid AND scores.friendcode = ? AND scores.scoregrade = "SSS+" AND charts.level = ?',[$id,$level]);
             $SSSP_data = $data[0]->SSSP;
+            $data = DB::select('SELECT count(scores.scoregrade) as "AP" from scores inner join charts on scores.chartid=charts.chartid AND scores.friendcode = ? AND scores.combograde = "AP" AND charts.level = ?',[$id,$level]);
+            $AP_data = $data[0]->AP;
+            $data = DB::select('SELECT count(scores.scoregrade) as "APP" from scores inner join charts on scores.chartid=charts.chartid AND scores.friendcode = ? AND scores.combograde = "AP+" AND charts.level = ?',[$id,$level]);
+            $APP_data = $data[0]->APP;
             $data = DB::select('SELECT avg(scores.score) as "avg" from scores inner join charts on scores.chartid=charts.chartid AND scores.friendcode = ? AND charts.level = ?',[$id,$level]);
             $avg_data = $data[0]->avg;
             $levelData = [
@@ -33,6 +37,8 @@ class ProfileController extends Controller
                 "SS+" => $SSP_data,
                 "SSS" => $SSS_data,
                 "SSS+" => $SSSP_data,
+                "AP" => $AP_data,
+                "AP+" => $APP_data,
                 "avg" => $avg_data,
             ];
             array_push($levelArray,$levelData);
--- maimai_viewer/resources/views/profile.blade.php
+++ maimai_viewer/resources/views/profile.blade.php
@@ -23,27 +23,38 @@
         <p>{{ $profile->email }}</p>
         <p>{{ $profile->friendcode }}</p>
         <p>{{ $profile->rating }}</p> --}}
-            <table class="table table-bordered table-dark">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Level</th>
-                        <th scope="col">SS+</th>
-                        <th scope="col">SSS</th>
-                        <th scope="col">SSS+</th>
-                        <th scope="col">Average Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($levelArray as $level)
-                        <tr>
-                            <th scope="row">{{ $level["Level"]}}</th>
-                            <td>{{ $level["SS+"] }}</td>
-                            <td>{{ $level["SSS"] }}</td>
-                            <td>{{ $level["SSS+"] }}</td>
-                            <td>{{ $level["avg"] }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="bg-limit"
+            style="background: linear-gradient(rgba(0, 0, 0, 0.40), rgba(0, 0, 0, 0.40)), url({{ asset('images/nav_bg/bg_img.jpg') }}), no-repeat;">
+            <div class="row justify-content-center"  style="padding-top: 1.5em;">
+                <div class="col-auto">
+                    <table class="table table-bordered table-dark">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">Level</th>
+                                <th scope="col">SS+</th>
+                                <th scope="col">SSS</th>
+                                <th scope="col">SSS+</th>
+                                <th scope="col">AP</th>
+                                <th scope="col">AP+</th>
+                                <th scope="col">Average Score</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($levelArray as $level)
+                                <tr>
+                                    <th scope="row">{{ $level['Level'] }}</th>
+                                    <td>{{ $level['SS+'] }}</td>
+                                    <td>{{ $level['SSS'] }}</td>
+                                    <td>{{ $level['SSS+'] }}</td>
+                                    <td>{{ $level['AP'] }}</td>
+                                    <td>{{ $level['AP+'] }}</td>
+                                    <td>{{ $level['avg'] }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </main>
 @endsection
