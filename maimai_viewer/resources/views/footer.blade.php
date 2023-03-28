3859aefbe5e66cca3e36fe8dac4876f2ab765dc0
OhsterTohster <59667279+OhsterTohster@users.noreply.github.com>
2023-03-29 05:00:40 +0800

    edit footer

--- maimai_viewer/resources/views/footer.blade.php
+++ maimai_viewer/resources/views/footer.blade.php
@@ -2,7 +2,7 @@
     <hr class="footer_hr">
     <div class="info">Support us @ <br> <a href="https://github.com/AlephaLIm/maimai_php" class="foot_info">https://github.com/AlephaLIm/maimai_php</a>!</div>
     <div class="devs">
-        <div class="dev_label">Contributers: </div>
-        <div class="dev_ppl">OhsterTohster @@Github<br>Alepha @@Github<br>brdley @@Github<br>SimYanZhe @@Github<br>Nydrel2 @@Github</div>
+        <div class="dev_label">We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with Sega, or any of its subsidiaries or its affiliates.</div>
+        
     </div>
 </div>
