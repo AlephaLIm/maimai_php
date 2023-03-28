01e53c7d41b1f5049b5251f550e472cab1dede16
OhsterTohster <59667279+OhsterTohster@users.noreply.github.com>
2023-03-28 21:28:05 +0800

    Squashed commit of the following:
    commit 56a9815d13db7d105f1a2a144fbf3267f34c3dd6
    Merge: ffdf9a8 a6a3188
    Author: OhsterTohster <59667279+OhsterTohster@users.noreply.github.com>
    Date:   Tue Mar 28 21:27:04 2023 +0800
    
        Merge branch 'main' into javier
    
    commit ffdf9a8fc0e6161bb62c0948ad6117f19806885e
    Author: OhsterTohster <59667279+OhsterTohster@users.noreply.github.com>
    Date:   Tue Mar 28 21:25:48 2023 +0800
    
        Create ProfileController.php

---
+++ maimai_viewer/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+
+
+class ProfileController extends Controller
+{
+    public function updateProfile(Request $request)
+    {
+        
+
+        
+        //Obtains the relevant JSON object which stores it in an array of objects
+        
+        
+        $userData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'new_email' => 'email|unique:users,email',
+            'new_password' => [
+                'confirmed',
+                Password::min(8)
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised()],
+            ]);
+        $friendcode = $request->user()->friendcode;
+        $validate = DB::select("SELECT * FROM users WHERE email = ? AND password = ?", [$userData['email'],bcrypt($userData['password'])]);
+    
+         
+        if (count($validate) == 1) {
+
+            if ($userData['new_email'] != null) {
+                DB::update("UPDATE users SET email = ? WHERE friendcode = ?",[$userData['email'],$friendcode]);
+            }
+            
+            if ($userData['new_password'] != null) {
+                DB::update("UPDATE users SET password = ? WHERE friendcode = ?",[bcrypt($userData['password']),$friendcode]);
+
+            }
+
+
+            return redirect()->back()->with("emailsuccess","Email updated successfully")->with("passwordsuccess","Password updated successfully");
+        }
+        else {
+            return redirect()->back()->withErrors(['email' => 'Email does not match','password' => 'Password does not match']);
+        }
+        
+        
+        
+        // Return a JSON response with the new user's data
+        
+    }
+ 
+}
