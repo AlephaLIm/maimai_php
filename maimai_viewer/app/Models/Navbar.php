1c0ce863d8881f6f7cc046e8a5b5941f843189a6
Jun Hao Lim <s10195387@connect.np.edu.sg>
2023-03-28 23:44:06 +0800

    added friendcode to nav

--- maimai_viewer/app/Models/Navbar.php
+++ maimai_viewer/app/Models/Navbar.php
@@ -6,14 +6,16 @@ class Navbar {
     public $name;
     public $title;
     public $rating;
+    public $friendcode;
     public $profile_img;
     
-    public static function retrieveuser(?string $name = null, ?string $title = null, ?string $rating = null, ?string $img = null) {
+    public static function retrieveuser(?string $name = null, ?string $title = null, ?string $rating = null, ?string $img = null,?string $friendcode = null) {
         $user = new Navbar();
         $user->name = $name ?? " Guest_User";
         $user->title = $title ?? "-- No Title --";
         $user->rating = $rating ?? "------";
         $user->profile_img = $img ?? URL::asset("/images/nav_icons/guest.jpg");
+        $user->friendcode = $friendcode;
 
         return $user;
     }
